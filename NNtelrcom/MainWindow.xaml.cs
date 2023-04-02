bea865160e132a2e0c98d48043725a56cc03726a
иван матвеев <ivat1056@gmail.com>
2023-04-02 17:48:47 +0300

    Добавьте файлы проекта.

---
+++ NNtelrcom/MainWindow.xaml.cs
@@ -0,0 +1,32 @@
+﻿using NNtelrcom.Class;
+using NNtelrcom.Pages;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace NNtelrcom
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            FrameClass.frame = frame;
+            FrameClass.frame.Navigate(new MainPage());
+        }
+    }
+}
