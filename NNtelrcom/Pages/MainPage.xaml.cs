a04609bfd190d86129f7c78111714d409e8ebb72
иван матвеев <ivat1056@gmail.com>
2023-04-02 19:54:38 +0300

    dialogwindow

--- NNtelrcom/Pages/MainPage.xaml.cs
+++ NNtelrcom/Pages/MainPage.xaml.cs
@@ -12,6 +12,9 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Globalization;
+using Microsoft.Win32;
+using System.IO;
 
 namespace NNtelrcom.Pages
 {
@@ -20,9 +23,38 @@ namespace NNtelrcom.Pages
     /// </summary>
     public partial class MainPage : Page
     {
+
         public MainPage()
         {
             InitializeComponent();
+
+
+
+        }
+
+        private void file_Click(object sender, RoutedEventArgs e)
+        {
+            int size = -1;
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Multiselect = true;
+            openFileDialog.Filter = "Text files (*.log)|*.log|All files (*.*)|*.*";
+            openFileDialog.InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+            if (openFileDialog.ShowDialog() == true)
+            {
+                foreach (string filename in openFileDialog.FileNames) lbFiles.Items.Add(System.IO.Path.GetFileName(filename));
+                string file = openFileDialog.FileName;
+                try
+                {
+                    string text = File.ReadAllText(file);
+                    size = text.Length;
+                }
+                catch (IOException)
+                {
+                }
+                
+            }
+            
+
         }
     }
 }
