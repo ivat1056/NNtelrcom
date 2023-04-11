dfee7002b83ed3f06fd7533d1eed154f0065454f
иван матвеев <ivat1056@gmail.com>
2023-04-11 15:07:17 +0300

    11.04.2023

---
+++ NNtelrcom/Windows/LoadingWindow.xaml.cs
@@ -0,0 +1,72 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace NNtelrcom.Windows
+{
+    /// <summary>
+    /// Логика взаимодействия для LoadingWindow.xaml
+    /// </summary>
+    public partial class LoadingWindow : Window
+    {
+        public LoadingWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            
+                int size = -1;
+                OpenFileDialog openFileDialog = new OpenFileDialog();
+                openFileDialog.Multiselect = true;
+                openFileDialog.Filter = "Text files (*.log)|*.log|All files (*.*)|*.*";
+                openFileDialog.InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+                if (openFileDialog.ShowDialog() == true)
+                {
+                    foreach (string filename in openFileDialog.FileNames) lbFiles.Items.Add(System.IO.Path.GetFileName(filename));
+                    string file = openFileDialog.FileName;
+
+                    using (StreamReader reader = new StreamReader(file))
+                    {
+                        string line;
+
+                        while ((line = reader.ReadLine()) != null)
+                        {
+                            string[] massiv = new string[37];
+                            massiv = line.Split(';');
+                            string a = massiv[37];
+                        }
+                    }
+
+                }
+
+
+           
+        }
+
+        private void ADD_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Back_Click(object sender, RoutedEventArgs e)
+        {
+            MainMenu mainMenu = new MainMenu();
+            this.Close();
+            mainMenu.Show();
+        }
+    }
+}
--- NNtelrcom/Windows/MainMenu.xaml.cs
+++ NNtelrcom/Windows/MainMenu.xaml.cs
@@ -1,41 +1,34 @@
-﻿using NNtelrcom.Class;
-using NNtelrcom.Windows;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Controls.Primitives;
 using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace NNtelrcom
+namespace NNtelrcom.Windows
 {
     /// <summary>
-    /// Логика взаимодействия для MainWindow.xaml
+    /// Логика взаимодействия для MainMenu.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainMenu : Window
     {
-        public MainWindow()
+        public MainMenu()
         {
             InitializeComponent();
-            
         }
 
-        private void Button_Click(object sender, RoutedEventArgs e)
+        private void BtnUploy_Click(object sender, RoutedEventArgs e)
         {
-        
-            MainMenu mainMenu = new MainMenu();
+            LoadingWindow loading = new LoadingWindow();
             this.Close();
-            mainMenu.Show();
-
+            loading.Show();
         }
     }
 }
